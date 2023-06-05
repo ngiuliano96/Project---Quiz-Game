0105ae0c150ab83a5a4afbf70fa1673753056d87
ngiuliano96 <ngiuliano1996@gmail.com>
2023-06-05 17:30:18 -0400

    Initial commit

---
+++ data.py
@@ -0,0 +1,130 @@
+question_data = [
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "Igneous rocks are formed by excessive heat and pressure.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "A person can get sunburned on a cloudy day.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "The 'Gympie Stinger\' is the deadliest plant in the world.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Steel is an alloy of Iron and Carbon.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "A defibrillator is used to start up a heartbeat once a heart has stopped beating.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Type 1 diabetes is a result of the liver working improperly.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "hard",
+        "question": "It was once believed that injecting shark cartilage "
+                    "into people would prevent them from contracting cancer.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Anatomy considers the forms of macroscopic structures such as organs and organ systems.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "An atom contains a nucleus.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Scientists accidentally killed the once known world\'s oldest living creature, "
+                    "a mollusc, known to be aged as 507 years old.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "An exothermic reaction is a chemical reaction that releases energy by radiating electricity.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "In the periodic table, Potassium\'s symbol is the letter K.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "An Astronomical Unit is the distance between Earth and the Moon.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium", "question": "Sugar contains fat.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "An average human can go two weeks without water.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "hard",
+        "question": "The value of one Calorie is different than the value of one calorie.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "Celiac Disease is a disease that effects the heart, "
+                    "causing those effected to be unable to eat meat.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "Psychology is the science of behavior and mind.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Pneumonoultramicroscopicsilicovolcanoconiosis is a synonym for "
+                    "the disease known as silicosis.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "Water always boils at 100 deg C, 212 deg F, 373.15K, no matter where you are.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "Sound can travel through a vacuum.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "Not including false teeth; A human has two sets of teeth in their lifetime.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "A plant that has a life cycle for more than a year is known as an annual.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "The most frequent subconscious activity repeated by the human body is blinking.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "medium",
+        "question": "'Tachycardia\' or \'Tachyarrhythmia' "
+                    "refers to a resting heart-rate near or over 100 BPM.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]}]
